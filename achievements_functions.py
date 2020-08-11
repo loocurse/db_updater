9d1c9ee6eb7c8b2a1817d6c69802bde9e96c502a
Lucas Ng <lucas_ng@mymail.sutd.edu.sg>
2020-08-12 00:24:06 +0800

    edited functions to return points num

--- achievements_functions.py
+++ achievements_functions.py
@@ -1,23 +1,27 @@
 import database_read_write
 import pandas as pd
 
+points = pd.read_csv('./tables_csv/achievement_points.csv', index_col=['achievement'])['points'].to_dict()
+
 
 def _lower_energy_con(user_id):
     """Achievement: Clock a lower energy consumption than yesterday"""
     df = database_read_write.get_energy_ytd_today(user_id)
     list = df.groupby(by='date').sum()['power'].to_list()
-    return True if list[0] > list[1] else False
+    return points['lower_energy_con'] if list[0] > list[1] else 0
 
 
 def _turn_off_leave(user_id):
     """Achievement: Turn off your plug loads when you leave your desk for a long period of time during the day"""
     # Approach: check if plug loads are switched off when presence is not detected
-    return False
+    condition = True
+    return points['turn_off_leave'] if condition else 0
 
 
 def _turn_off_end(user_id):
     """Achievement: Turn off your plug loads during at the end of the day"""
-    return False
+    condition = True
+    return points['turn_off_end'] if condition else 0
 
 
 def _cost_saving(user_id):
@@ -25,12 +29,13 @@ def _cost_saving(user_id):
     week_view = database_read_write.read_cost_savings()
     week_view_user = week_view[week_view.user_id == user_id]
     list = week_view_user['total'][-2:].to_list()
-    return True if list[1] > list[0] else False
+    return points['cost_saving'] if list[1] > list[0] else 0
 
 
 def _schedule_based(user_id):
     """Achievement: Set next week's schedule-based controls"""
-    return False
+    condition = True
+    return points['schedule_based'] if condition else 0
 
 
 def _complete_daily(user_id):
@@ -40,42 +45,49 @@ def _complete_daily(user_id):
     else:
         df = database_read_write.get_daily_table()
         df = df.loc[df.user_id == user_id]
-        return True if sum(df['complete_all'].to_list()) > 4 else False
+        return points['complete_daily'] if sum(df['complete_all_daily'].to_list()) >= 20*4 else 0
 
 
 def _tree_first(user_id):
     """Achievement: Save your first tree"""
-    return False
+    condition = True
+    return points['tree_first'] if condition else 0
 
 
 def _tree_fifth(user_id):
     """Achievement: Save your fifth tree"""
-    return False
+    condition = True
+    return points['tree_fifth'] if condition else 0
 
 
 def _tree_tenth(user_id):
     """Achievement: Save your tenth tree"""
-    return False
+    condition = True
+    return points['tree_tenth'] if condition else 0
 
 
 def _redeem_reward(user_id):
     """Achievement: Redeem your first reward from your rewards page"""
-    return False
+    condition = True
+    return points['redeem_reward'] if condition else 0
 
 
 def _first_remote(user_id):
     """Achievement: Try out our remote control feature for the first time"""
-    return False
+    condition = True
+    return points['first_remote'] if condition else 0
 
 
 def _first_schedule(user_id):
     """Achievement: Set your first schedule-based setting"""
-    return False
+    condition = True
+    return points['first_schedule'] if condition else 0
 
 
 def _first_presence(user_id):
     """Achievement: Set your first presence-based setting"""
-    return False
+    condition = True
+    return points['first_presence'] if condition else 0
 
 
 def achievements_update_hourly():
@@ -92,8 +104,8 @@ def achievements_update_hourly():
             if not status:  # if task not yet met, check if task met
                 updated_output.append(daily_functions[num](user_id))
             else:
-                updated_output.append(True)
-        updated_output.append(True) if all(updated_output) else updated_output.append(False)
+                updated_output.append(status)
+        updated_output.append(points['complete_daily']) if all(updated_output) else updated_output.append(0)
         # Replace information
         user_df.at[today, 'lower_energy_con':'complete_all'] = updated_output
         output = pd.concat([output, user_df])
@@ -116,7 +128,7 @@ def achievements_update_daily():
                 updated_output.append(weekly_functions[num](user_id))
             else:
                 updated_output.append(True)
-        updated_output.append(True) if all(updated_output) else updated_output.append(False)
+        updated_output.append(points['complete_weekly']) if all(updated_output) else updated_output.append(0)
         user_df.loc[0, 'cost_saving':'complete_weekly'] = updated_output
         output = pd.concat([output, user_df], ignore_index=True)
     database_read_write.update_db(output, 'achievements_weekly')
