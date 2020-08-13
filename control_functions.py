676e522616db9415d98e87f70817577cfa6cfd1e
raymondlow <cocker123>
2020-08-13 18:21:34 +0800

    Included function for schedule based control

--- control_functions.py
+++ control_functions.py
@@ -2,6 +2,7 @@ import psycopg2
 import pandas as pd
 import requests
 from requests.auth import HTTPBasicAuth
+from datatime import datetime
 
 
 def check_remote_control():
@@ -27,9 +28,9 @@ def check_remote_control():
 
 
     def activate_remote_control(meter_id, command):
-        query = requests.get('http://{}/api/devices/{}'.format(fibaro_address, meter_id),
-                             auth=HTTPBasicAuth(fibaro_username, fibaro_password)).json()
-        print(query)
+        # query = requests.get('http://{}/api/devices/{}'.format(fibaro_address, meter_id),
+        #                      auth=HTTPBasicAuth(fibaro_username, fibaro_password)).json()
+        # print(query)
 
         # return true if device is on, false if device is off
         return None
@@ -57,7 +58,8 @@ def check_remote_control():
         if len(diff) != 0:
             # Identify meter id based on device type and user id and switch it ON/OFF
             for user_id, device_type, new_state, previous_state in diff:
-                cursor.execute("".format())
+                cursor.execute("SELECT meter_id FROM power_energy_consumption WHERE user_id={} AND device_type={} "
+                               "ORDER BY unix_time DESC LIMIT 1".format(user_id, device_type.lower()))
                 meter_id = cursor.fetchone()[0]
 
                 if new_state is True and previous_state is False:
@@ -125,13 +127,13 @@ def update_device_state():
             for i in range(len(meter_ids)):
                 # Find device type based on meter id
                 cursor.execute("SELECT user_id, device_type FROM power_energy_consumption WHERE meter_id={} "
-                               "ORDER BY unix_time DESC LIMIT 1;".format(meter_ids[i]))
+                               "ORDER BY unix_time DESC LIMIT 1".format(meter_ids[i]))
                 user_id, device_type = cursor.fetchone()[0]
                 device_type = device_type.capitalize()
 
                 # Update device state based on user id and device type
                 cursor.execute("UPDATE plug_mate_app_remotedata SET device_state={} WHERE user_id={} AND "
-                               "device_type={}".format(device_states[i], user_id, device_type))
+                               "device_type={}".format(device_states[i], user_id, device_type.capitalize()))
 
             connection.commit()
 
@@ -180,23 +182,85 @@ def schedule_control():
     before switching off any devices, do an additional check to see if user is round.
     Checking frequency: 15 minutes
     """
+
+    # Database and Fibaro credentials
+    # user = 'dadtkzpuzwfows'
+    # database_password = '1a62e7d11e87864c20e4635015040a6cb0537b1f863abcebe91c50ef78ee4410'
+    # host = 'ec2-46-137-79-235.eu-west-1.compute.amazonaws.com'
+    # port = '5432'
+    # database = 'd53rn0nsdh7eok'
+    user = 'raymondlow'
+    database_password = 'password123'
+    host = 'localhost'
+    port = '5432'
+    database = 'plug_mate_dev_db'
+    fibaro_address = '172.19.243.58:80'
+    fibaro_username = 'admin'
+    fibaro_password = 'admin'
+
+
+    def activate_remote_control(meter_id, command):
+        # query = requests.get('http://{}/api/devices/{}'.format(fibaro_address, meter_id),
+        #                      auth=HTTPBasicAuth(fibaro_username, fibaro_password)).json()
+        # print(query)
+
+        # return true if device is on, false if device is off
+        return None
+
+
+    def check_schedule(state):
+        if state == 'On':
+            event_column = 'event_start'
+        else:
+            event_column = 'event_end'
+
+        # Obtain user id and device type information for devices that needs to be switched ON/OFF
+        control_schedule = [(schedules.loc[i, 'user_id'], schedules.loc[i, 'device_type'])
+                            for i in range(len(schedules))
+                            if current_time in schedules.loc[i, event_column] and day_of_week in schedules.loc[i, 'event_rrule']]
+
+        # Remotely switch ON/OFF devices using meter id (obtained using user id and device type)
+        if state == 'On':
+            command = 'turnOn'
+        else:
+            command = 'turnOff'
+        for user_id, device_type in control_schedule:
+            cursor.execute("SELECT meter_id FROM power_energy_consumption WHERE user_id={} AND device_type={} "
+                           "ORDER BY unix_time DESC LIMIT 1".format(user_id, device_type.lower()))
+            meter_ids = cursor.fetchall()
+
+            for meter_id in meter_ids:
+                activate_remote_control(meter_id, command)
+
+
     # Obtain the schedule for all users and device types
+    try:
+        # Connect to PostgreSQL database
+        connection = psycopg2.connect(user=user, password=database_password, host=host,
+                                      port=port, database=database)
+        cursor = connection.cursor()
 
-    # Check if the starting time of any schedule matches with the current time
-        # If found, then check if the device is already ON
-            # If device is ON, then do nothing
+        # Obtain schedules from database
+        cursor.execute("SELECT * FROM plug_mate_app_scheduledata")
+        query_result = cursor.fetchall()
+        schedules = pd.DataFrame(query_result, columns=[desc[0] for desc in cursor.description])
 
-            # Else, switch on device
+        # Check if the starting time of any schedule matches with the current time
+        current_time = datetime.today().strftime('%H:%M')
+        day_of_week = datetime.today().strftime('%A')
 
-        # Else, do nothing
+        # Check schedule to see if any devices needs to be switched ON/OFF
+        check_schedule('On')
+        check_scheduld('Off')
 
-    # Check if ending time of any schedule matches with current time
-        # If found, check if user is present at the desk for the last 5 minutes
-            # If user is present, then do nothing
+    except(Exception, psycopg2.Error) as error:
+        if (connection):
+            print('Error: ', error)
 
-            # Else, switch off device based on device type, user id, and meter id
-
-        # Else, do nothing
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
 
     return None
 
