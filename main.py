2ce6e4df2d06b88f155fe42793914aa9566f209b
Lucas Ng <lucas_ng@mymail.sutd.edu.sg>
2020-08-26 23:58:13 +0800

    added checker for daily presence and schedule

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     schedule.every().hour.at(':15').do(achievements_to_update, ['turn_off_leave'])
     schedule.every().hour.at(':30').do(achievements_to_update, ['turn_off_leave'])
     schedule.every().hour.at(':45').do(achievements_to_update, ['turn_off_leave'])
-    schedule.every().day.at("00:05").do(achievements_to_update, ['turn_off_leave'])
+    schedule.every().day.at("00:05").do(achievements_to_update, ['daily_schedule', 'daily_presence'])
 
-    for time in ['23:50', '23:55']:
+    for time in ['23:50', '23:55', '00:05']:
         if time == '23:50':
             checklist = ['lower_energy_con', 'turn_off_end', 'tree_first', 'tree_fifth', 'tree_tenth', ]
         else:
